03e4044efecf751f78231943f3e0050933cfbf14
Ziaad03 <ziadd.essam03@gmail.com>
2024-04-25 01:06:49 +0200

    add the navigation bar and style it

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+ 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        
+      </Routes>
+      
+      
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
